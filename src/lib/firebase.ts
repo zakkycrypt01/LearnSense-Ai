a0ced4aff3587fe37d321cafc707dfdd9b7c9323
zakkycrypt01 <zakkycrypt@gmail.com>
2025-07-29 07:51:12 +0100

    feat: update firebase configuration to use environment variables for sensitive data

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  "projectId": "learnsense-ai-3x7be",
-  "appId": "1:731268515924:web:efd25b77aa26088e4305c1",
-  "storageBucket": "learnsense-ai-3x7be.firebasestorage.app",
-  "apiKey": "AIzaSyCNk6wt_oDN3MElTBSROPtL2BZLgfYzULc",
-  "authDomain": "learnsense-ai-3x7be.firebaseapp.com",
-  "messagingSenderId": "731268515924"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
